c1d46efcb009690313648427e7b2ce3552de94e3
Ope Afolabi <38186457+gbopola@users.noreply.github.com>
2023-10-10 17:59:22 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://product-feedback-app-inhn.onrender.com",
+        target: "http://localhost:5000",
         secure: false,
       },
     },
