82cda05a63f286301b4ad065feb7ce6a8dd56878
gbopola <opeafolabi5@gmail.com>
2023-10-12 10:44:09 +0100

    served frontend build as static assets

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ connectDB();
 
 const app = express();
 
+// Serve your frontend build as static assets
+app.use(express.static(path.join(__dirname, "public")));
+
+// Handle all other requests by serving the index.html for your frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
