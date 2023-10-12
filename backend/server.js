3cb6fa1e43aa4b1effdbd012f2263e2aa4c20dc6
gbopola <opeafolabi5@gmail.com>
2023-10-12 10:48:27 +0100

    added path variable

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 connectDB();
 
