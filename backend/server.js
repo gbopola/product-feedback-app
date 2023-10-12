48a8b9d14b581d58d9897b0c722fc37f6c90a7e0
gbopola <opeafolabi5@gmail.com>
2023-10-12 16:24:45 +0100

    added error handler

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Handle all other requests by serving the index.html for your frontend
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist"));
 });
 
